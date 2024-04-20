b6043633beeda4e009f2044cf82c3110e08e44d6
mknakamura26 <mknakamura26@amherst.edu>
2024-04-20 09:58:40 -0400

    Update DATASET.R

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(tidytext)
 BillboardLyrics <-  
-  readr::read_csv("/Users/mianakamura/GitHub/BillboardLyrics/data-raw/billboard_lyrics_1964_2015.csv") |>
+  readr::read_csv("billboard_lyrics_1964_2015.csv") |>
   janitor::clean_names()
 usethis::use_data(BillboardLyrics, overwrite = TRUE)
 
